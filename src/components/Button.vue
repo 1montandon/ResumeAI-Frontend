ebceb92b8515e54f4a1b35492a08e4d26924059d
1montandon <emontandon11@gmail.com>
2025-06-21 01:02:30 -0300

    feat: add Button component and update Header with navigation buttons

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'primary', // 'primary' or 'secondary'
+  },
+  to: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+})
+
+const router = useRouter()
+
+const buttonClass = computed(() =>
+  props.type === 'primary'
+    ? 'bg-[#0D80F2] px-7 py-3 rounded-lg font-semibold text-sm text-[#F7FAFC] hover:cursor-pointer active:scale-90 ease-out duration-300'
+    : 'bg-[#E8EDF5] px-7 py-3 rounded-lg font-semibold text-sm text-[#0D141C] hover:cursor-pointer active:scale-90 ease-out duration-300',
+)
+
+function handleClick() {
+  if (props.to) {
+    router.push(props.to)
+  }
+}
+</script>
+<template>
+  <button :class="buttonClass" @click="handleClick">
+    {{ text }}
+  </button>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import Button from './Button.vue';
 </script>
 
 <template>
   <header class="w-screen bg-white border-b border-[#E5E8EB] ">
     <div class="flex h-18  items-center justify-between px-12 ">
     <div>
-      <h1>teste</h1>
+      <h1 class="text-lg font-bold">Resume AI</h1>
     </div>
     <nav >
-      <ul class="flex flex-row">
-        <li>teste</li>
-        <li>teste</li>
-        <li>teste</li>
+      <ul class="flex flex-row gap-10 items-center">
+        <li class="font-medium text-[#0D141C]">Product</li>
+        <li class="font-medium text-[#0D141C]">Pricing</li>
+        <li class="font-medium text-[#0D141C]">Resources</li>
+        <li class="flex flex-row gap-5">
+          <Button text="Teste" type="primary" to="/"></Button>
+           <Button text="Teste" type="secundary" to="/"></Button>
+      </li>
       </ul>
     </nav>
     </div>
